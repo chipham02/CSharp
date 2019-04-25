9ffd09cd56e7719aa9f73acb6d4d47e0c1c86549
Chi Pham <chigau02@gmail.com>
2019-04-25 18:26:37 +0700

    Add project files.

---
+++ CSharp/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Cart
+    {
+        public int id;
+        public String customer;
+        public int grandTotal;
+        public List<String> productList;
+        public String city;
+        public String country;
+
+        public void nhapThongTin()
+        {
+            Console.WriteLine("Id: ");
+            id = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Customer: ");
+            customer = Console.ReadLine();
+            Console.WriteLine("Grandtotal: ");
+            grandTotal = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("City: ");
+            city = Console.ReadLine();
+            Console.WriteLine("Country: ");
+            country = Console.ReadLine();
+            Console.WriteLine("ProductList: ");
+            productList.Add(Console.ReadLine());
+        }
+
+        public void inThongTin()
+        {
+            Console.WriteLine("ID: " + id + "Customer: " + customer + "GrandTotal: " + grandTotal + "City: " + city + "Country:  " + country);
+        }
+
+        public void addProduct()
+        {
+
+        }
+
+        public void delProduct()
+        {
+
+        }
+
+        public void Grandtotal()
+        {
+
+        }
+
+    }
+}
---
+++ CSharp/Product.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Product
+    {
+        public int id;
+        public String name;
+        public double price;
+        public int qty;
+        public String image;
+        public String desc;
+        public List<String> gallery;
+
+        public Product(int id, string name, double price, int qty, string image, string desc, List<String> gallery)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+            this.gallery = gallery;
+        }
+
+        public Product(int x)
+        {
+
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("Nhập id: ");
+            id = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập tên: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Nhập giá: ");
+            price = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập số lượng: ");
+            qty = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập image: ");
+            image = Console.ReadLine();
+            Console.WriteLine("Desc: ");
+            desc = Console.ReadLine();
+            Console.WriteLine("Gallery: ");
+            gallery.Add(Console.ReadLine());
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine("ID: " + id + "Tên: " + name + "Giá: " + price + "Số lượng: " + qty + "Image: " + image + "Gallery: " + gallery);
+        }
+
+        public void checkQty()
+        {
+            if (qty > 0)
+            {
+                Console.WriteLine("Con hang");
+            }
+            Console.WriteLine("Het hang");
+        }
+
+        public void addGallery(String image)
+        {
+            
+            
+        }
+
+        public void delGallery(String image)
+        {
+
+        }
+    }
+}
